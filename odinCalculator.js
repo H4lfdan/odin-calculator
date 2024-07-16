4c4c802364073a33683fb71e3f8ae1d3ef7fafbd
H4lfdan <danrobnett@gmail.com>
2024-07-16 16:44:28 -0500

    Ensure only one decimal point per input. Fixed bugs resulting in NaN output. Punishment for dividing by 0.

--- odinCalculator.js
+++ odinCalculator.js
@@ -1,32 +1,174 @@
 const output = document.getElementById('output');
-function add() {
-    const x = parseInt(document.getElementById('inputOne').value);
-    const y = parseInt(document.getElementById('inputTwo').value);
-    let z = x + y;
-    output.innerHTML = z;
-    console.log(z);
+output.innerHTML = 0;
+let inputA = '';
+let inputB = '';
+let operator = '';
+let answer = '';
+let hasOperator = false;
+let hasDecimal = false;
+
+function typeNum(string) {
+    if(!hasOperator) {
+        if (answer != '') {
+            answer = '';
+            inputA = '';
+        }
+        if (string === '.') {
+            if (hasDecimal) {
+                return;
+            } else if (!hasDecimal) {
+                hasDecimal = true;
+            }
+        }
+        inputA += string;
+        output.innerHTML = inputA;
+    } else if(hasOperator) {
+        if (string === '.') {
+            if (hasDecimal) {
+                return;
+            } else if (!hasDecimal) {
+                hasDecimal = true;
+            }
+        }
+        inputB += string;
+        output.innerHTML = inputB;
+    }
 }
 
-function subtract() {
-    const x = parseInt(document.getElementById('inputOne').value);
-    const y = parseInt(document.getElementById('inputTwo').value);
-    let z = x - y;
-    output.innerHTML = z;
-    console.log(z);
+function typeOper(string) {
+    if (inputB != '') {
+        if (inputA === '' || inputA === NaN) {
+            inputA = '0';
+        }
+        operate();
+    }
+    operator = string;
+    hasOperator = true;
+    hasDecimal = false;
+    console.log(inputA);
 }
 
-function multiply() {
-    const x = parseInt(document.getElementById('inputOne').value);
-    const y = parseInt(document.getElementById('inputTwo').value);
-    let z = x * y;
-    output.innerHTML = z;
-    console.log(z);
+function operate() {
+    const a = parseFloat(inputA);
+    const b = parseFloat(inputB);
+
+    if(operator === '+') {
+        answer = a + b;
+    } else if (operator === '-') {
+        answer = a - b;
+    } else if (operator === '*') {
+        answer = a * b;
+    } else if (operator === '/') {
+        if (inputB === '0') {
+            answer = 'wanker';
+        } else {
+            answer = a / b;
+        }
+    } else {
+        console.log('what the heck');
+    }
+
+    console.log(answer);
+
+    let lengthCheck = ('' + answer).split('.');
+    if (lengthCheck.length === 2) {
+        if (lengthCheck[1].length >= 4) {
+            answer = parseFloat(answer).toFixed(3);
+            console.log(answer);
+        }
+    }
+
+    output.innerHTML = answer;
+    console.log(inputA , operator , inputB , answer);
+    inputA = answer;
+    inputB = '';
+    operator = '';
+    hasOperator = false;
+
+    if (answer === 'wanker' || answer === NaN) {
+        inputA = '0';
+    }
 }
 
-function divide() {
-    const x = parseInt(document.getElementById('inputOne').value);
-    const y = parseInt(document.getElementById('inputTwo').value);
-    let z = x / y;
-    output.innerHTML = z;
-    console.log(z);
+function equals() {
+    if (operator === '' || inputB === '') {
+        output.innerHTML = inputA;
+        inputB = '';
+        operator = '';
+        hasOperator = false;
+    } else {
+        operate();
+    }
 }
+
+function resetEquation() {
+    inputA = '';
+    inputB = '';
+    operator = '';
+    output.innerHTML = 0;
+    hasOperator = false;
+    hasDecimal = false;
+    console.log('cleared');
+}
+
+//  *   *   *   *   *
+
+// const output = document.getElementById('output');
+// let displayValue = '';
+// let hasOper = false;
+// let typingNum = false;
+
+// function operate() {
+//     const equation = displayValue.split(' ');
+//     const a = parseInt(equation[0]);
+//     const operator = equation[1];
+//     const b = parseInt(equation[2]);
+//     let answer = '';
+
+//     if(operator === '+') {
+//         answer = a + b;
+//     } else if (operator === '-') {
+//         answer = a - b;
+//     } else if (operator === '*') {
+//         answer = a * b;
+//     } else if (operator === '/') {
+//         answer = a / b;
+//     } else {
+//         console.log('what the heck');
+//     }
+
+//     resetEquation(answer);
+//     console.log(equation);
+//     console.log(a , operator , b)
+//     hasOper = false;
+// }
+
+// function typeNum(string) {
+//     if(!typingNum) {
+//         output.innerHTML = '';
+//         typingNum = true;
+//     }
+
+//     output.innerHTML += string;
+//     displayValue += string;
+// }
+
+// function typeOper(string) {
+//     if(hasOper) {
+//         operate();
+//         displayValue += string;
+//     } else {
+//         output.innerHTML = string;
+//         displayValue += string;
+//     }
+
+//     hasOper = true;
+//     typingNum = false;
+// }
+
+// function resetEquation(int) {
+//     output.innerHTML = int;
+//     displayValue = int;
+//     typingNum = false;
+//     console.log(displayValue);
+// }
